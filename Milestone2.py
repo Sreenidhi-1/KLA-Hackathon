29caa7dacc0837f75972187260ade84d6134d822
Sreenidhi-1 <sreenidhirajavel1@gmail.com>
2024-01-12 17:02:22 +0530

    Milestone2 Partially Completed

--- Milestone2.py
+++ Milestone2.py
@@ -24,13 +24,13 @@ print(total_boxes)
 
 def condition(x,y):
     flag=0
-    if((x>radius or x<-radius )and (y>radius or y<-radius)):
+    if((x>=radius or x<=(-1*radius) )and (y>=radius or y<=(-1*radius))):
         flag+=1
-    if((x+die_size_x>radius or x+die_size_x<-radius) and (y>radius or y<-radius)):
+    if((x+die_size_x>=radius or x+die_size_x<=(-1*radius)) and (y>=radius or y<=(-1*radius))):
         flag+=1
-    if((x>radius or x<-radius) and (y+die_size_y>radius or y+die_size_y<-radius)):
+    if((x>=radius or x<=(-1*radius)) and (y+die_size_y>radius or y+die_size_y<=(-1*radius))):
         flag+=1
-    if((x+die_size_x>radius or x+die_size_x<-radius) and (y+die_size_y>radius or y+die_size_y<-radius)):
+    if((x+die_size_x>=radius or x+die_size_x<=(-1*radius)) and (y+die_size_y>=radius or y+die_size_y<=(-1*radius))):
         flag+=1
     if(flag==4):
         return False
@@ -49,67 +49,150 @@ def bfs(start_x,start_y):
     res=[(0,0)]
     visited.append((start_x,start_y))
     c=0
+    r=[(start_x-h,start_y-k)]
     while(True):
         x,y=queue.pop(0)
         l1,l2=from_reference.pop(0)
         if(condition(x,y)==False):
-            visited.remove((x,y))
-            res.remove((l1,l2))
+
             break
         if((x+die_size_x,y) not in visited):
             queue.append([x+die_size_x,y])
             from_reference.append([l1+1,l2])
             res.append((l1+1,l2))
             visited.append((x+die_size_x,y))
+            r.append((x+die_size_x,y))
         if((x-die_size_x,y) not in visited):
             queue.append([x-die_size_x,y])
             from_reference.append([l1-1,l2])
             res.append((l1-1,l2))
             visited.append((x-die_size_x,y))
+            r.append((x-die_size_x,y))
         if((x,y+die_size_y) not in visited):
             queue.append([x,y+die_size_y])
             from_reference.append([l1,l2+1])
             res.append((l1,l2+1))
+            r.append((x,y+die_size_y))
             visited.append((x,y+die_size_y))
         if((x,y-die_size_y) not in visited):
             queue.append([x,y-die_size_y])
             from_reference.append([l1,l2-1])
             res.append((l1,l2-1))
             visited.append((x,y-die_size_y))
+            r.append((x,y-die_size_y))
         if((x+die_size_x,y+die_size_y) not in visited):
             queue.append([x+die_size_x,y+die_size_y])
             from_reference.append([l1+1,l2+1])
             res.append((l1+1,l2+1))
             visited.append((x+die_size_x,y+die_size_y))
+            r.append((x+die_size_x,y+die_size_y))
         if((x+die_size_x,y-die_size_y) not in visited):
             queue.append([x+die_size_x,y-die_size_y])
             from_reference.append([l1+1,l2-1])
             res.append((l1+1,l2-1))
             visited.append((x+die_size_x,y-die_size_y))
+            r.append((x+die_size_x,y-die_size_y))
         if((x-die_size_x,y-die_size_y) not in visited):
             queue.append([x-die_size_x,y-die_size_y])
             from_reference.append([l1-1,l2-1])
             res.append((l1-1,l2-1))
             visited.append((x-die_size_x,y-die_size_y))
+            r.append((x-die_size_x,y-die_size_y))
         if((x-die_size_x,y+die_size_y) not in visited):
             queue.append([x-die_size_x,y+die_size_y])
             from_reference.append([l1-1,l2+1])
             res.append((l1-1,l2+1))
             visited.append((x-die_size_x,y+die_size_y))
+            r.append((x-die_size_x,y+die_size_y))
         c+=1
         #print(queue,from_reference)
     return res,visited
 
+"""def bfs(start_x,start_y):
+    queue=[]
+    from_reference=[]
+    visited=[]
+    start_x=start_x
+    start_y=start_y
+    print(start_x,start_y)
+    from_reference.append([0,0])
+    queue.append([start_x-h,start_y-k])
+    res=[(0,0)]
+    visited.append((start_x-h,start_y-k))
+    c=0
+    while(True):
+        x,y=queue.pop(0)
+        l1,l2=from_reference.pop(0)
+        if(len(visited)>total_boxes):
+            break
+        if(condition(x,y)==False):
+            visited.remove((x,y))
+            res.remove((l1,l2))
+            break
+        if((x+die_size_x-h,y-k) not in visited):
+            queue.append([x+die_size_x-h,y-k])
+            from_reference.append([l1+1,l2])
+            res.append((l1+1,l2))
+            visited.append((x+die_size_x-h,y-k))
+        if((x-die_size_x-h,y-k) not in visited):
+            queue.append([x-die_size_x-h,y-k])
+            from_reference.append([l1-1,l2])
+            res.append((l1-1,l2))
+            visited.append((x-die_size_x-h,y-k))
+        if((x-h,y+die_size_y-k) not in visited):
+            queue.append([x-h,y+die_size_y-k])
+            from_reference.append([l1,l2+1])
+            res.append((l1,l2+1))
+            visited.append((x-h,y+die_size_y-k))
+        if((x-h,y-die_size_y-k) not in visited):
+            queue.append([x-h,y-die_size_y-k])
+            from_reference.append([l1,l2-1])
+            res.append((l1,l2-1))
+            visited.append((x-h,y-die_size_y-k))
+        if((x+die_size_x-h,y+die_size_y-k) not in visited):
+            queue.append([x+die_size_x-h,y+die_size_y-k])
+            from_reference.append([l1+1,l2+1])
+            res.append((l1+1,l2+1))
+            visited.append((x+die_size_x-h,y+die_size_y-k))
+        if((x+die_size_x-h,y-die_size_y-k) not in visited):
+            queue.append([x+die_size_x-h,y-die_size_y-k])
+            from_reference.append([l1+1,l2-1])
+            res.append((l1+1,l2-1))
+            visited.append((x+die_size_x-h,y-die_size_y-k))
+        if((x-die_size_x-h,y-die_size_y-k) not in visited):
+            queue.append([x-die_size_x-h,y-die_size_y-k])
+            from_reference.append([l1-1,l2-1])
+            res.append((l1-1,l2-1))
+            visited.append((x-die_size_x-h,y-die_size_y-k))
+        if((x-die_size_x-h,y+die_size_y-k) not in visited):
+            queue.append([x-die_size_x-h,y+die_size_y-k])
+            from_reference.append([l1-1,l2+1])
+            res.append((l1-1,l2+1))
+            visited.append((x-die_size_x-h,y+die_size_y-k))
+        c+=1
+        #print(queue,from_reference)
+    return res,visited"""
 
 
 res,visited=bfs(start_x,start_y)
+print(len(res))
+coordinate=[]
+points=[]
+
+"""for i in range(len(visited)):
+    if(condition(visited[i][0],visited[i][1])==True):
+        points.append(visited[i])
+        coordinate.append(res[i])"""
+
+print(len(points))
+print(len(visited))
 result=[]
 for i in range(len(res)):
     s=str(res[i]).replace(' ', '')+":"+str(visited[i]).replace(' ','')
     result.append(s)
-print(result)
+#print(result)
 #writing in file
-with open('milestone2_output4.txt', 'w') as f:
+with open('milestone2_output4a.txt', 'w') as f:
     for i in result:
         f.write(i)
         f.write('\n')
